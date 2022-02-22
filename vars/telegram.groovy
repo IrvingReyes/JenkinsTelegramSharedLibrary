7f771fef85e9f43e9e5251f4ae69e866c0429468
IrvingReyes <ve00386@innovaccion.mx>
2022-02-22 12:40:24 -0600

    Cambiamos el archivo telegram.groovy a mensajeTelegram.groovy

--- vars/telegram.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(String mensaje = '', String SecretoToken = 'human', String SecretoChatID = '' ) {
-  withCredentials([string(credentialsId: SecretoToken, variable: 'TOKEN'),string(credentialsId: SecretoChatID, variable: 'CHATID') ]) {
-            sh """
-            curl -v -X POST --silent --output /dev/null https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHATID -d text='${mensaje}'
-            """
-            } 
-}
