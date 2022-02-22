bb0ec308887c047b571c2df05bec67cba8189f45
IrvingReyes <78865644+IrvingReyes@users.noreply.github.com>
2022-02-22 13:19:54 -0600

    Update mensajeTelegram.groovy

--- vars/mensajeTelegram.groovy
+++ vars/mensajeTelegram.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String mensaje = '', String SecretoToken = 'human', String SecretoChatID = '' ) {
+def call(String mensaje = '', String SecretoToken = '', String SecretoChatID = '' ) {
   withCredentials([string(credentialsId: SecretoToken, variable: 'TOKEN'),string(credentialsId: SecretoChatID, variable: 'CHATID') ]) {
             sh """
             curl -v -X POST --silent --output /dev/null https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHATID -d text='${mensaje}'
